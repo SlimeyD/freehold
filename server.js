5eae77ba523c33e1a23b3393b0d153fcbe0d6ad9
simontegg <simontegg@localhost.localdomain>
2016-06-08 16:32:57 +1200

    add pull-streams

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 'use strict'
 const http = require('http')
+const websocket = require('websocket-stream')
 const serveStatic = require('serve-static')
 const finalHandler = require('finalhandler')
+const toPull = require('stream-to-pull-stream')
+const pull = require('pull-stream')
+
+// variables
 const port = process.env.PORT || 3000
-const websocket = require('websocket-stream')
+const connections = {}
+const messages = []
+let connectionIdCounter = 0
 
 // setup
 const serve = serveStatic('public')
@@ -15,18 +22,22 @@ const server = http.createServer((req, res) => {
 })
 
 const wss = websocket.createServer({ server: server }, stream => {
-  stream.on('data', d => {
-    console.log('data', d.toString())
-  })
-
-  console.log('stream', stream.socket)
-
-//  stream.socket.onopen(connection => {
- //   console.log('open', connection)
- // })
-
+  pull(
+    toPull.source(stream),
+    pull.log()
+  )
 })
 
+wss.on('connection', connection => {
+  connection.id = connectionIdCounter ++
+  connections[connection.id] = connection
+  console.log(`${new Date()} Connection Id: ${connection.id} accepted`)
+
+  connection.on('close', () => {
+    console.log(`${new Date()} Connection Id: ${connection.id} closed`)
+    delete connections[connection.id]
+  })
+})
 
 
 // listen
