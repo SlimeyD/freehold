dad3338b4e75010e18ca036f0cd981a528f64ed0
simontegg <simon@enspiral.com>
2016-07-06 12:31:52 +1200

    failing submit email test

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -14,7 +14,22 @@ module.exports = function() {
     browser.click('button=Click here to begin')
   })
 
+  this.When('I enter my email: "$string"', email => {
+    browser.setValue('input[type="email"]', email)
+  })
+
+  this.When('I click "$string"', text => {
+    browser.click(`=${text}`)
+  })
+
   this.Then('the page changes to "$string"', (pathname, cb) => {
     assert.equal(browser.getUrl(), `${url}${pathname}`, cb)
+  })
+
+  this.Then(
+    'I see a notification telling me: "$string"', (message, cb) => {
+    const notification = browser.element('.notification')
+    notification.waitForExist(5000)
+    assert.equal(notification.getText(), message, cb)
   })
 }
--- features/submit-email.feature
+++ features/submit-email.feature
@@ -1,7 +1,8 @@
 Feature: submit my email
-
+  
+  @watch
   Scenario: 
     Given I am on the "/offset" page 
-    When I enter my email
-      And click "Find out more"
-    Then I see a notification telling me I will be contacted shortly
+    When I enter my email: "johnny5@alive.com"
+      And I click "Find out more"
+      Then I see a notification telling me: "Thanks! you will be contacted shortly"
